19955795aaaabafb383aca65386317871bc7f375
robmal-0 <robmal-0@student.ltu.se>
2020-09-23 19:55:59 +0200

    v0.2.2
    Swedish translation (teachers demand) and some additional functionality.

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+function writeGameState() {
+	$("#year").html(`Rapport för år: ${gameState.year}`);
+	$("#starved").html(`Döda i svält: ${gameState.starved}`);
+	$("#newcomers").html(`Nykommande: ${gameState.newcomers}`);
+	$("#population").html(`Befolkning: ${gameState.population}`);
+	$("#acres").html(`Mark: ${gameState.acres}`);
+	$("#bushels").html(`Matkorgar: ${gameState.bushels}`);
+	$("#harvest").html(`Körde: ${gameState.harvest}`);
+	$("#rats").html(`Råttor: ${gameState.rats}`);
+	$("#price").html(`Pris: ${gameState.price}`);
+
+	updateSliderValueOutput("sliderAcrestosellbuy", "outputAcrestosellbuy");
+	updateSliderValueOutput("sliderFeedpeople", "outputFeedpeople");
+	updateSliderValueOutput("sliderPlantwithseed", "outputPlantwithseed");
+}
+
 $(() => {
 	$("div.tab").first().css("display", "initial");
 	$("menu p").on("click", function () {
@@ -7,5 +23,36 @@ $(() => {
 	$("input[type='range']").on("dblclick", function () {
 		this.value = 0;
 		$(this).trigger("change");
+	});
+	$("legend span").attr("contenteditable", "");
+	$("legend span").on("focusout", function () {
+		let slider = this.parentNode.parentNode.querySelector(
+			"input[type='range']"
+		);
+
+		let val;
+		try {
+			val = parseInt(eval($(this).text()));
+		} catch (e) {
+			val = 0;
+		}
+		let max = parseInt(slider.max);
+		let min = parseInt(slider.min);
+		val = val <= max ? val : max;
+		val = val >= min ? val : min;
+		$(this).html(val);
+		slider.value = val;
+	});
+	$("legend span").on("keydown", function (e) {
+		if (e.keyCode == 13) {
+			e.preventDefault();
+			this.blur();
+		} else if (
+			![8, 37, 39, 187, 189].includes(e.keyCode) &&
+			(e.keyCode < 48 || e.keyCode > 57) &&
+			!(e.keyCode == 191 && e.shiftKey)
+		) {
+			e.preventDefault();
+		}
 	});
 });
