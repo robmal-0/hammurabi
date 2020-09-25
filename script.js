0aa85a207d62e022255a0c28841839c1b6f793f0
robmal-0 <robmal-0@student.ltu.se>
2020-09-25 11:53:49 +0200

    v0.4.0
    Minor changes

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function writeGameState() {
 	$("#population").html(`<p>Befolkning:</p><p>${gameState.population}</p>`);
 	$("#acres").html(`<p>Mark:</p><p>${gameState.acres}</p>`);
 	$("#bushels").html(`<p>Skäppor:<p>${gameState.bushels}</p>`);
-	$("#harvest").html(`<p>Körde:</p><p>${gameState.harvest}</p>`);
+	$("#harvest").html(`<p>Skörd:</p><p>${gameState.harvest}</p>`);
 	$("#rats").html(`<p>Råttor:</p><p>${gameState.rats}</p>`);
 	$("#price").html(`<p>Pris:</p><p>${gameState.price}</p>`);
 
